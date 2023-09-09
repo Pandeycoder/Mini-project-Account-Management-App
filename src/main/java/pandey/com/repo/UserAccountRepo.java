e7ab202f39b8e89eab64208a3113333d0497ce4b
pandeycoder <pandeycoder8474@gmail.com>
2023-09-09 16:40:22 +0530

    upadte repo

--- src/main/java/pandey/com/repo/UserAccountRepo.java
+++ src/main/java/pandey/com/repo/UserAccountRepo.java
@@ -4,17 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 import jakarta.transaction.Transactional;
 import pandey.com.entities.UserAccount;
-
 public interface UserAccountRepo extends JpaRepository<UserAccount,Integer>{
 	
 	@Modifying
 	@Transactional
 	@Query("update UserAccount set activeSw=:status where userId=:userId")
 	public void updateUserAccountStatus(Integer userId,String status);
-	
-	
-
 }
